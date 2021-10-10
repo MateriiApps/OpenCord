b651db6c08332ed4e1df2c32c155a40b06528d13
X1nto <burtkni@yandex.ru>
2021-10-10 19:04:32 +0400

    fix missing deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,10 +88,12 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.1.1")
 
     implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.material:material-icons-core:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui-util:$composeVersion")
     implementation("androidx.compose.ui:ui:$composeVersion")
 
     implementation("io.coil-kt:coil-compose:1.4.0")
