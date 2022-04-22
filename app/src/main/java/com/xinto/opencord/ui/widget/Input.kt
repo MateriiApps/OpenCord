d1df7880b17449220675ae1a4dd503186bba61d1
X1nto <burtkni@yandex.ru>
2022-04-22 15:49:18 +0400

    replace custom icon button with material3

--- app/src/main/java/com/xinto/opencord/ui/widget/Input.kt
+++ app/src/main/java/com/xinto/opencord/ui/widget/Input.kt
@@ -6,12 +6,12 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Send
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.xinto.opencord.ui.component.FilledIconButton
 import com.xinto.opencord.ui.component.OCBasicTextField
 
 @Composable
