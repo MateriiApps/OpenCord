65bf50ed9e8336d47daa53b7dcd522395957449b
X1nto <burtkni@yandex.ru>
2022-05-08 15:26:18 +0400

    add partial data generation via ksp

---
+++ partialgen/src/main/java/com/xinto/partialgen/PartialProcessorProvider.kt
@@ -0,0 +1,12 @@
+package com.xinto.partialgen
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class PartialProcessorProvider : SymbolProcessorProvider {
+
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        return PartialProcessor(environment.codeGenerator)
+    }
+}
