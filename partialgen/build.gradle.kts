2324ad23c30636a0730c716e1319f44770b256e1
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-05-11 07:02:34 -0700

    ExperimentalSerializationApi yop

--- partialgen/build.gradle.kts
+++ partialgen/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
@@ -7,4 +9,8 @@ dependencies {
     implementation(project(":ksp-util"))
     implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+}
+
+tasks.withType(KotlinCompile::class.java) {
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
 }
