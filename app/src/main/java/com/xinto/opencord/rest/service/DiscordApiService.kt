da08ef5437c976449fe39893a4175a0df185ae84
X1nto <burtkni@yandex.ru>
2022-05-12 18:23:34 +0400

    fix message reactions url

--- app/src/main/java/com/xinto/opencord/rest/service/DiscordApiService.kt
+++ app/src/main/java/com/xinto/opencord/rest/service/DiscordApiService.kt
@@ -245,7 +245,7 @@ class DiscordApiServiceImpl(
             emote: String
         ): String {
             val channelMessagesUrl = getChannelMessagesUrl(channelId)
-            return "$channelId/$messageId/$emote/@me"
+            return "$channelMessagesUrl/$channelId/$messageId/$emote/@me"
         }
     }
 }
