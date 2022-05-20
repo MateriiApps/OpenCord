e13251f83e4c78a08e96e881a61501c3d6a4f61a
X1nto <burtkni@yandex.ru>
2022-05-20 21:50:32 +0400

    cleanup

--- app/src/main/java/com/xinto/opencord/ui/viewmodel/CurrentUserViewModel.kt
+++ app/src/main/java/com/xinto/opencord/ui/viewmodel/CurrentUserViewModel.kt
@@ -53,8 +53,9 @@ class CurrentUserViewModel(
         }
         gateway.onEvent<UserSettingsUpdateEvent> {
             val mergedData = userSettings?.merge(it.data.toDomain())
-                .also { mergedData -> userSettings = mergedData }
-            //TODO fix unsetting values bug by fixing the merger
+                .also { mergedData ->
+                    userSettings = mergedData
+                }
             userStatus = mergedData?.status
             userCustomStatus = mergedData?.customStatus?.text
             println(mergedData?.customStatus?.text)
@@ -66,7 +67,7 @@ class CurrentUserViewModel(
                 userSettings = settings
                 userStatus = settings.status
                 userCustomStatus = settings.customStatus?.text
-                state = State.Loaded
+//                state = State.Loaded
             } catch (e: Throwable) {
                 e.printStackTrace()
                 state = State.Error
--- app/src/main/java/com/xinto/opencord/ui/widget/Channel.kt
+++ app/src/main/java/com/xinto/opencord/ui/widget/Channel.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 
 @Composable
