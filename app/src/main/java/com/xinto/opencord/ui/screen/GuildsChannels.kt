9d7ca27a4313846ad7117fda00ae3b49b3115a4b
X1nto <burtkni@yandex.ru>
2022-05-20 22:30:13 +0400

    add skeleton for guild name

--- app/src/main/java/com/xinto/opencord/ui/screen/GuildsChannels.kt
+++ app/src/main/java/com/xinto/opencord/ui/screen/GuildsChannels.kt
@@ -415,6 +415,32 @@ private fun ChannelsListLoading(
     val shimmer = rememberShimmer(shimmerBounds = ShimmerBounds.View)
     Column(modifier = modifier) {
         val items = remember { (5..20).random() }
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(14.dp)
+        ) {
+            val guildName = remember { " ".repeat((20..30).random()) }
+            val shimmerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .shimmer(shimmer)
+                    .clip(CircleShape)
+                    .background(shimmerColor)
+            )
+            Text(
+                modifier = Modifier
+                    .shimmer(shimmer)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(shimmerColor),
+                text = guildName,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
         repeat(items) { itemIndex ->
             val isCategory = remember { itemIndex == 0 || (0..6).random() == 1 }
             if (isCategory) {
@@ -424,13 +450,13 @@ private fun ChannelsListLoading(
                         bottom = 4.dp
                     ),
                     title = {
-                        val spaces = remember { (10..20).random() }
+                        val title = remember { " ".repeat((10..20).random()) }
                         Text(
                             modifier = Modifier
                                 .shimmer(shimmer)
                                 .clip(MaterialTheme.shapes.medium)
-                                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)),
-                            text = " ".repeat(spaces)
+                                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)),
+                            text = title
                         )
                     },
                     icon = {
@@ -439,7 +465,7 @@ private fun ChannelsListLoading(
                                 .fillMaxSize()
                                 .shimmer(shimmer)
                                 .clip(CircleShape)
-                                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f))
+                                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f))
                         )
                     },
                     onClick = {},
@@ -448,13 +474,13 @@ private fun ChannelsListLoading(
                 WidgetChannelListItem(
                     modifier = Modifier.padding(bottom = 2.dp),
                     title = {
-                        val spaces = remember { (15..30).random() }
+                        val title = remember { " ".repeat((15..30).random()) }
                         Text(
                             modifier = Modifier
                                 .shimmer(shimmer)
                                 .clip(MaterialTheme.shapes.medium)
                                 .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)),
-                            text = " ".repeat(spaces)
+                            text = title
                         )
                     },
                     icon = {
