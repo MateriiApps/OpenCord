1ff180da39ec439b66e7b0ecea02747c614c8892
X1nto <burtkni@yandex.ru>
2022-05-31 23:57:03 +0400

    xinto moment

--- app/src/main/java/com/xinto/opencord/ui/MainActivity.kt
+++ app/src/main/java/com/xinto/opencord/ui/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.xinto.opencord.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.SideEffect
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.xinto.opencord.rest.dto.ApiMessagePartial
-import com.xinto.opencord.rest.dto.ApiSnowflake
 import com.xinto.opencord.ui.screen.MainRootScreen
 import com.xinto.opencord.ui.theme.OpenCordTheme
 import com.xinto.opencord.ui.viewmodel.MainViewModel
-import com.xinto.partialgen.PartialValue
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToStream
 import org.koin.android.ext.android.get
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +17,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val d = ApiMessagePartial(id = PartialValue.Value(ApiSnowflake(0UL)))
-        val t = Json.encodeToString(d)
-        Log.i("t", t)
         setContent {
             OpenCordTheme(darkMode = true) {
                 val systemUiController = rememberSystemUiController()
