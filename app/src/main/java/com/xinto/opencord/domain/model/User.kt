d1615870f5d49f06f2ef1c41d689517d5f5133fd
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-06-01 07:04:18 -0700

    update user domain model

--- app/src/main/java/com/xinto/opencord/domain/model/User.kt
+++ app/src/main/java/com/xinto/opencord/domain/model/User.kt
@@ -1,18 +1,61 @@
 package com.xinto.opencord.domain.model
 
-data class DomainUser(
-    val id: ULong,
-    val username: String,
-    val discriminator: String,
-    val avatarUrl: String,
-    val bot: Boolean = false,
-) : Mentionable {
-    val tag: String
-        get() = "$username$formattedDiscriminator"
+sealed class DomainUser : Mentionable {
+    abstract val id: ULong
+    abstract val username: String
+    abstract val discriminator: String
+    abstract val avatarUrl: String
+    abstract val bot: Boolean
+    abstract val bio: String?
+
+    override val formattedMention: String
+        get() = "<@$id>"
 
     val formattedDiscriminator: String
         get() = "#$discriminator"
 
-    override val formattedMention: String
-        get() = "<@$id>"
+    val tag: String
+        get() = "$username$formattedDiscriminator"
 }
+
+data class DomainUserPublic(
+    override val id: ULong,
+    override val username: String,
+    override val discriminator: String,
+    override val avatarUrl: String,
+    override val bot: Boolean,
+    override val bio: String?,
+    val pronouns: String?,
+    val flags: Int, // TODO: implement bitfield
+) : DomainUser()
+
+data class DomainUserPrivate(
+    override val id: ULong,
+    override val username: String,
+    override val discriminator: String,
+    override val avatarUrl: String,
+    override val bot: Boolean,
+    override val bio: String?,
+    val pronouns: String?,
+    val flags: Int,
+    val mfaEnabled: Boolean,
+    val verified: Boolean,
+    val email: String,
+    val phone: String?, // TODO: verify this is nullable
+    val locale: String,
+) : DomainUser()
+
+data class DomainUserReadyEvent(
+    override val id: ULong,
+    override val username: String,
+    override val discriminator: String,
+    override val avatarUrl: String,
+    override val bot: Boolean,
+    override val bio: String?,
+    val mfaEnabled: Boolean,
+    val verified: Boolean,
+    val premium: Boolean,
+    val mobile: Boolean,
+    val desktop: Boolean,
+    val purchasedFlags: Int, // TODO: implement bitfield
+) : DomainUser()
