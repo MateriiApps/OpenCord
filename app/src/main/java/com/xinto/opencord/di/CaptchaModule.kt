73ba8c8f7754e7600f9d0f2d43a2bd802f6d8df4
X1nto <burtkni@yandex.ru>
2022-06-18 13:38:12 +0400

    move captcha key to buildconfig

--- app/src/main/java/com/xinto/opencord/di/CaptchaModule.kt
+++ app/src/main/java/com/xinto/opencord/di/CaptchaModule.kt
@@ -3,12 +3,13 @@ package com.xinto.opencord.di
 import android.content.Context
 import com.hcaptcha.sdk.HCaptcha
 import com.hcaptcha.sdk.HCaptchaConfig
+import com.xinto.opencord.BuildConfig
 import org.koin.dsl.module
 
 val captchaModule = module {
     fun provideHCaptcha(context: Context): HCaptcha {
         val config = HCaptchaConfig.builder()
-            .siteKey("f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34") // doubt this will ever change
+            .siteKey(BuildConfig.CAPTCHA_KEY) // doubt this will ever change
             .resetOnTimeout(true)
             .build()
         return HCaptcha.getClient(context).setup(config)
