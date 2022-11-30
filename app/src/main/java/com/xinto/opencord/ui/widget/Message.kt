8ecf937ba235f7017f5b9a151a73eb702052c54f
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-11-30 10:58:14 -0800

    extract string

--- app/src/main/java/com/xinto/opencord/ui/widget/Message.kt
+++ app/src/main/java/com/xinto/opencord/ui/widget/Message.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
@@ -232,7 +233,7 @@ fun WidgetMessageAuthor(
                     shape = MaterialTheme.shapes.extraSmall,
                 ) {
                     Text(
-                        "BOT",
+                        text = stringResource(R.string.chat_bot_label),
                         modifier = Modifier.padding(horizontal = 4.dp)
                     )
                 }
