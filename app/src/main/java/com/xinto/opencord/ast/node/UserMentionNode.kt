9e5b06c1ef790018f7254efc631066ed9b222294
X1nto <burtkni@yandex.ru>
2022-12-06 22:14:42 +0400

    fix compile

--- app/src/main/java/com/xinto/opencord/ast/node/UserMentionNode.kt
+++ app/src/main/java/com/xinto/opencord/ast/node/UserMentionNode.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import com.xinto.opencord.ui.theme.DiscordBlurple
+import com.xinto.opencord.ui.theme.Blurple
 import com.xinto.simpleast.Node
 
 class UserMentionNode<RC>(
@@ -14,8 +14,8 @@ class UserMentionNode<RC>(
     context(AnnotatedString.Builder)
     override fun render(renderContext: RC) {
         withStyle(SpanStyle(
-            color = DiscordBlurple,
-            background = DiscordBlurple.copy(alpha = 0.2f),
+            color = Blurple,
+            background = Blurple.copy(alpha = 0.2f),
             fontWeight = FontWeight.SemiBold
         )) {
             append("@$userId")
