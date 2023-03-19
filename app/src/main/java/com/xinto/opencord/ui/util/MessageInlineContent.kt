84b1058eb18762e1503a7225282bfc25e5b4ad50
rushii <33725716+rushiiMachine@users.noreply.github.com>
2023-03-19 10:01:58 -0700

    perf(MessageInlineContent): fix unstableness

--- app/src/main/java/com/xinto/opencord/ui/util/MessageInlineContent.kt
+++ app/src/main/java/com/xinto/opencord/ui/util/MessageInlineContent.kt
@@ -3,26 +3,32 @@ package com.xinto.opencord.ui.util
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
 import androidx.compose.ui.unit.sp
 import com.xinto.opencord.BuildConfig
 import com.xinto.opencord.ui.components.OCImage
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.persistentHashMapOf
 
 @Composable
-fun messageInlineContent(): Map<String, InlineTextContent> {
+fun messageInlineContent(): ImmutableMap<String, InlineTextContent> {
     val emoteSize = (LocalTextStyle.current.fontSize.value + 2f).sp
-    return mapOf(
-        "emote" to InlineTextContent(
-            placeholder = Placeholder(
-                width = emoteSize,
-                height = emoteSize,
-                placeholderVerticalAlign = PlaceholderVerticalAlign.Center,
-            ),
-        ) { emoteId ->
-            OCImage(
-                url = "${BuildConfig.URL_CDN}/emojis/$emoteId",
-            )
-        },
-    )
+
+    return remember(emoteSize) {
+        persistentHashMapOf(
+            "emote" to InlineTextContent(
+                placeholder = Placeholder(
+                    width = emoteSize,
+                    height = emoteSize,
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center,
+                ),
+            ) { emoteId ->
+                OCImage(
+                    url = "${BuildConfig.URL_CDN}/emojis/$emoteId",
+                )
+            },
+        )
+    }
 }
